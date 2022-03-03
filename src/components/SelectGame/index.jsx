d098399705e175a05f0671b10ab5cd0e194583db
LuigiMenez <ltc.menez@gmail.com>
2022-03-03 11:03:09 +0100

    Added call Axios, and npm axios

--- src/components/SelectGame/index.jsx
+++ src/components/SelectGame/index.jsx
@@ -1,17 +1,24 @@
-import rpgs from '../../db';
-// import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Container, Game } from './style';
-// import CharacterChoice from './components/CharacterChoice';
 
 export default function SelectGame() {
+  const [jdrs, setJdrs] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5050/`).then(({ data }) => {
+      setJdrs(data);
+    });
+  }, []);
+
   return (
     <Container>
       <h2>Les univers</h2>
       <div className="games">
-        {rpgs.map((rpg) => {
+        {jdrs.map((rpg) => {
           return (
-            <Game img={rpg.img} alt={rpg.title}>
-              <p>{rpg.title}</p>
+            <Game key={rpg.id} img={rpg.image} alt={rpg.name}>
+              <p>{rpg.name}</p>
             </Game>
           );
         })}
