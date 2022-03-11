021df3ba499f394d5b6faafab1eef67beaedeeb4
LuigiMenez <ltc.menez@gmail.com>
2022-03-11 16:29:52 +0100

    Added first modal in thd homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
+import useModal from 'hooks/useModal';
+import Modal from '../Modal';
 import SHeader from './style';
 
 export default function Header() {
+  const { isShowing, toggle } = useModal();
+
   return (
     <SHeader>
       <>
         <div>
-          <button type="button">Connexion</button>
+          <button type="button" onClick={toggle}>
+            Connexion
+          </button>
+          <Modal isShowing={isShowing} hide={toggle} />
           <a href="test">Inscrivez-vous</a>
         </div>
         <h1> La boite à perso !</h1>
