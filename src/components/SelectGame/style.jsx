2f82e3f6398ab3f1c124b16a0247225cd1e9c3a4
LuigiMenez <ltc.menez@gmail.com>
2022-03-14 15:04:03 +0100

    Modified css card game

--- src/components/SelectGame/style.jsx
+++ src/components/SelectGame/style.jsx
@@ -8,12 +8,13 @@ export const Container = styled.div`
     text-align: center;
     font-weight: bold;
     color: var(--main-color);
-    margin-top: 15px;
-    margin-bottom: 15px;
+    margin-top: 24px;
+    margin-bottom: 16px;
   }
   div {
     display: flex;
     justify-content: space-around;
+    margin: 1rem;
   }
 
   a {
@@ -30,19 +31,13 @@ export const Game = styled.div`
   background: url(${(props) => props.img});
   background-size: cover;
   background-repeat: no-repeat;
-  width: 22rem;
+  width: 25rem;
   height: 23vh;
   border: solid 4px var(--main-color);
   border-radius: 15px;
-  margin: auto;
   margin-top: 10px;
   margin-bottom: 10px;
   overflow: hidden;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 14vh;
   transition: transform 1s;
   :hover {
     transform: scale(1.1);
@@ -58,7 +53,6 @@ export const Game = styled.div`
     font-weight: bold;
     width: 100%;
     height: 20%;
-    grid-area: 2 / 2/ 3 / 2;
     background-color: #1f1d359c;
     color: var(--second-color);
     padding-top: 5px;
